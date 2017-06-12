f8ccb8dd30f57b6ca90e9836b8295b6130307ace
Inna Bezzub <inna_bezzub@epam.com>
2017-06-12 17:58:13 +0300

    resp calendar

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -8,6 +8,9 @@ import {Component} from '@angular/core';
 })
 
 export class CalendarComponent {
+    public isDaySelected: boolean = false;
+    public isWeekSelected: boolean = false;
+    public isMonthSelected: boolean = true;
     public weekDaysNames = [
         'Monday',
         'Tuesday',
@@ -18,17 +21,22 @@ export class CalendarComponent {
         'Sunday'
     ];
     public currentMonth = 'June';
+    public currentDate = 'June';
     public calendarData = [
-        [
+        {week:{
+            isCurrent:false,
+            days:[
             {month: 'may', date: 29},
             {month: 'may', date: 30},
             {month: 'may', date: 31},
             {month: 'June', date: 1},
             {month: 'June', date: 2},
             {month: 'June', date: 3},
-            {month: 'June', date: 4},
-        ],
-        [
+            {month: 'June', date: 4}]
+        }},
+        {week:{
+            isCurrent:false,
+            days:[
             {month: 'June', date: 5},
             {month: 'June', date: 6},
             {month: 'June', date: 7},
@@ -36,8 +44,10 @@ export class CalendarComponent {
             {month: 'June', date: 9},
             {month: 'June', date: 10},
             {month: 'June', date: 11},
-        ],
-        [
+        ]}},
+        {week:{
+            isCurrent:false,
+            days: [
             {month: 'June', date: 12},
             {month: 'June', date: 13},
             {month: 'June', date: 14},
@@ -45,17 +55,20 @@ export class CalendarComponent {
             {month: 'June', date: 16},
             {month: 'June', date: 17},
             {month: 'June', date: 18},
-        ],
-        [
-            {month: 'June', date: 19},
-            {month: 'June', date: 20},
-            {month: 'June', date: 21},
-            {month: 'June', date: 22},
-            {month: 'June', date: 23},
-            {month: 'June', date: 24},
-            {month: 'June', date: 25},
-        ],
-        [
+        ]}},
+        {week:{
+            isCurrent:true,
+            days:[ {month: 'June', date: 19},
+                {month: 'June', date: 20, isCurrentDay: true},
+                {month: 'June', date: 21},
+                {month: 'June', date: 22},
+                {month: 'June', date: 23},
+                {month: 'June', date: 24},
+                {month: 'June', date: 25}]
+        }},
+        {week:{
+            isCurrent:false,
+            days:[
             {month: 'June', date: 26},
             {month: 'June', date: 27},
             {month: 'June', date: 28},
@@ -63,8 +76,10 @@ export class CalendarComponent {
             {month: 'June', date: 30},
             {month: 'jul', date: 1},
             {month: 'jul', date: 2},
-        ],
-        [
+        ]}},
+        {week:{
+            isCurrent:false,
+            days:[
             {month: 'jul', date: 3},
             {month: 'jul', date: 4},
             {month: 'jul', date: 5},
@@ -72,9 +87,29 @@ export class CalendarComponent {
             {month: 'jul', date: 7},
             {month: 'jul', date: 8},
             {month: 'jul', date: 9},
-        ]
+        ]}}
     ];
 
-    getCurrentMonth(){}
+    arrangeSelectDay() {
+        this.isDaySelected = true;
+        this.isWeekSelected = false;
+        this.isMonthSelected = false;
+        this.currentDate = 'Monday 12';
+    }
+
+    arrangeSelectWeek() {
+        this.isDaySelected = false;
+        this.isWeekSelected = true;
+        this.isMonthSelected = false;
+        this.currentDate = '12 June - 17June';
+    }
+
+    arrangeSelectMonth() {
+        this.isDaySelected = false;
+        this.isWeekSelected = false;
+        this.isMonthSelected = true;
+        this.currentDate = this.currentMonth;
+    }
+
 }
 
--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -3,6 +3,8 @@ import {UserService} from '../services/user.service';
 import {User} from '../../models/userModel';
 import {NotificationService} from '../services/notification.service';
 import {ValidationService} from "../services/validation.service";
+import {AuthenticationService} from "../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'registration-form',
@@ -22,25 +24,33 @@ export class RegistrationComponent {
 
     constructor(private userService: UserService,
                 private notification: NotificationService,
-                private validationService: ValidationService
-    ) {
+                private validationService: ValidationService,
+                private authenticationService: AuthenticationService,
+                private router: Router) {
     }
 
     onCreate() {
         if (this.isValid(this.user.password, this.confirmPassword)) {
             this.userService.create(this.user).subscribe(
                 () => {
-                    this.notification.success('Welcome! You create an account.')
+                    this.notification.success('Welcome! You create an account.');
+                    this.authenticationService.login(this.user.username, this.user.password).subscribe(
+                        () => {
+                            this.router.navigate(['/home']);
+                        },
+                        error => {
+                            this.notification.error(error._body);
+                        })
                 },
                 err => {
                     this.notification.error(err._body);
                 });
-        }else {
+        } else {
             this.notification.error('Wrong confirmation password');
         }
     }
 
-    isValid(pas1:string|number, pas2:string|number): boolean {
+    isValid(pas1: string|number, pas2: string|number): boolean {
         return this.validationService.isEqualPassword(pas1, pas2);
     }
 }
